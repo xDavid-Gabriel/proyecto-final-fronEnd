b48e5afc2cf17d09f3e127623643e272413d6f26
xDavid-Gabriel <davidbetalleluz31@gmail.com>
2021-12-04 12:14:03 -0500

    update Gabriel

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@
 export default function Footer() {
     return (
         <footer className="container">
-            <div className="footer-cont d-flex  position-absolute ">
+            <div className="footer-cont d-flex justify-content-center align-items-center">
                 <div className="copy me-2">
                     <p className="text-primary mb-0 fs-6">© HIDEB todos los derechos reservados</p>
                 </div>
